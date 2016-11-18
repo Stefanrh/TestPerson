d111786aedb17b28c525bfc77e9d0416417b26d8
Stefan Rishøj Hansen <stef6635@edu.easj.dk>
2016-11-18 11:44:51 +0100

    Created 2 methods to test the TestPersons name and age.

---
+++ ConsoleApplication8/Model/TestPerson.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8.Model
+{
+    public class TestPerson
+    {
+        string someName;
+
+
+        public string SomeName
+        {
+            get { return someName; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Sometext must not be empty or null");
+                }
+                someName = value;
+            }
+        }
+
+        int someAge;
+
+        public int SomeAge
+        {
+            get { return someAge; }
+            set
+            {
+                if (value < 0 || 130 > value)
+                {
+                    throw new ArgumentException("somenumber must be in range 0-130");
+                }
+                someAge = value;
+            }
+        }
+    }
+}
